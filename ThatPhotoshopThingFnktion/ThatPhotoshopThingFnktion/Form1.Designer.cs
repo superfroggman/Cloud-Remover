052bb64cfe08c0d8f0c5a41b4bf1c20f59edf7a7
superfroggman <49985901+superfroggman@users.noreply.github.com>
2019-04-25 15:15:49 +0200

    Add files via upload

---
+++ ThatPhotoshopThingFnktion/ThatPhotoshopThingFnktion/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ThatPhotoshopThingFnktion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CheckboxSlowmode = new System.Windows.Forms.CheckBox();
+            this.ButtonFilepath = new System.Windows.Forms.Button();
+            this.TextboxSavename = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(0, 26);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 1;
+            this.ButtonStart.Text = "Start Process";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Location = new System.Drawing.Point(12, 52);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(85, 13);
+            this.LabelStatus.TabIndex = 3;
+            this.LabelStatus.Text = "Waiting For Files";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(497, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CheckboxSlowmode
+            // 
+            this.CheckboxSlowmode.AutoSize = true;
+            this.CheckboxSlowmode.Location = new System.Drawing.Point(12, 68);
+            this.CheckboxSlowmode.Name = "CheckboxSlowmode";
+            this.CheckboxSlowmode.Size = new System.Drawing.Size(94, 17);
+            this.CheckboxSlowmode.TabIndex = 5;
+            this.CheckboxSlowmode.Text = "slow but pretty";
+            this.CheckboxSlowmode.UseVisualStyleBackColor = true;
+            this.CheckboxSlowmode.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // ButtonFilepath
+            // 
+            this.ButtonFilepath.Location = new System.Drawing.Point(0, 0);
+            this.ButtonFilepath.Name = "ButtonFilepath";
+            this.ButtonFilepath.Size = new System.Drawing.Size(75, 23);
+            this.ButtonFilepath.TabIndex = 6;
+            this.ButtonFilepath.Text = "Choose Files";
+            this.ButtonFilepath.UseVisualStyleBackColor = true;
+            this.ButtonFilepath.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TextboxSavename
+            // 
+            this.TextboxSavename.Location = new System.Drawing.Point(6, 91);
+            this.TextboxSavename.Name = "TextboxSavename";
+            this.TextboxSavename.Size = new System.Drawing.Size(100, 20);
+            this.TextboxSavename.TabIndex = 7;
+            this.TextboxSavename.Text = "save name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 366);
+            this.Controls.Add(this.TextboxSavename);
+            this.Controls.Add(this.ButtonFilepath);
+            this.Controls.Add(this.CheckboxSlowmode);
+            this.Controls.Add(this.LabelStatus);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cloud Remover";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox CheckboxSlowmode;
+        private System.Windows.Forms.Button ButtonFilepath;
+        private System.Windows.Forms.TextBox TextboxSavename;
+    }
+}
+
---
+++ ThatPhotoshopThingFnktion/ThatPhotoshopThingFnktion/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ThatPhotoshopThingFnktion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool slowmode = false;
+        string[] filePaths;
+        Bitmap bmp;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //making sure there are images
+            if (filePaths == null)
+            {
+                return;
+            }
+
+            changeStatus("Processing");
+
+            Bitmap[] bmps = new Bitmap[filePaths.Length];
+            
+            for(int i = 0; i < filePaths.Length; i++)
+            {
+                bmps[i] = new Bitmap(Image.FromFile(filePaths[i]));
+            }
+            bmp = bmps[0];
+            pictureBox1.Image = bmp;
+            this.Update();
+
+            //For each image(except the first)
+            for (int i = 1; i < filePaths.Length; i++)
+            {
+                //For each height row
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    //For each width row
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        //Get current and new pixel and change to the brightest one
+                        Color currentPixel = bmp.GetPixel(x, y);
+                        Color newPixel = bmps[i].GetPixel(x, y);
+                        if ((newPixel.R+newPixel.B+newPixel.G) < (currentPixel.R + currentPixel.B + currentPixel.G))
+                        {
+                            bmp.SetPixel(x, y, newPixel);
+                        }
+                    }
+                    //Update image every height row if slowmode is activated
+                    if (slowmode)
+                    {
+                        pictureBox1.Image = bmp;
+                        this.Refresh();
+                    }
+                }
+                //Update image after each image processed
+                pictureBox1.Image = bmp;
+                
+                this.Refresh();
+
+            }
+            changeStatus("DONE!");
+            ButtonFilepath.Text = "Save";
+            renderFinished = true;
+            this.Refresh();
+        }
+
+        void changeStatus(string text)
+        {
+            LabelStatus.Text = text;
+            this.Refresh();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            slowmode = !slowmode;
+        }
+
+        bool renderFinished = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (renderFinished)
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        //changeStatus("Image Saved");
+                        changeStatus("Saved to: " + fbd.SelectedPath + "\\" + TextboxSavename.Text + ".png");
+                        
+                        bmp.Save(fbd.SelectedPath + "\\" + TextboxSavename.Text + ".png");
+                    }
+                    else
+                    {
+                        changeStatus("Save failed sucessfully");
+                    }
+                    ButtonFilepath.Text = "Choose Files";
+                    renderFinished = false;
+                    this.Refresh();
+                }
+            }
+            else
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        filePaths = Directory.GetFiles(fbd.SelectedPath, "*.png");
+                        changeStatus(filePaths.Length.ToString() + " files chosen");
+                    }
+                }
+            }          
+        }
+    }
+}
